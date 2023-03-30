ad38cc9cc37288034fe3dbca85058d9d7c545b15
xjx <2869418079@qq.com>
2023-03-30 09:06:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# android Project 2 后端
+***
+## 文档
+***
+[安卓实验二答辩报告文档](https://cmjhgnav4v.feishu.cn/docx/TSv9dfxJpoXgU6xzi4ucUnprnbh)
+
+## 使用
+### 配置数据库
+自行在 config 文件夹下创建本地数据库配置文件 conf_db.go ,作为配置本地MySql和Redis文件
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	router.InitRouter(r)
+
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
